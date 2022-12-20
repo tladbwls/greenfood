f4144b8e1e16024b48f96cb43d53aa1985c9820d
shimyoujin <zipte@naver.com>
2022-12-20 23:49:29 +0900

    first

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+  //   loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     clickable: true,
+  //     dynamicBullets: true,
+  //   },
+});
