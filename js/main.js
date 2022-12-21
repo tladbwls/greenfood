daea0d7747e662ae57c0675faf1b06a67c42723d
tladbwls <zipte@naver.com>
2022-12-21 12:36:19 +0900

    slider

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 var swiper = new Swiper(".mySwiper", {
-  //   loop: true,
+  loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  speed: 1000,
+
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
+  // loop: false,
+  // loopAdditionalSlides: 1,
   //   pagination: {
   //     el: ".swiper-pagination",
   //     clickable: true,
