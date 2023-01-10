a53412b47aae377c03c4b467ee2c1681efb01002
tladbwls <zipte@naver.com>
2023-01-10 13:01:10 +0900

    나옴...

--- js/main.js
+++ js/main.js
@@ -126,56 +126,27 @@ const panel = document.querySelectorAll(".nav-menu-bar");
 //   });
 // }
 
-// for (let i = 0; i < panel.length; i++) {
-//   const panels = panel[i];
-//   console.log(panels);
-// }
 btns.forEach(function (a) {
+  console.log(a);
   a.addEventListener("click", function () {
     this.classList.toggle("active");
+    // this.sibling.classList.add("active");
+    if (this.classList.contains("active")) {
+      console.log(this.nextSibling.nextSibling);
+      this.nextSibling.nextSibling.classList.add("active");
+    } else {
+      this.nextSibling.nextSibling.classList.remove("active");
+    }
   });
-  if (btns.classList.contains("active")) {
-    panel.classList.add("active");
-  } else {
-    panel.classList.remove("active");
-  }
+  // panel.forEach(function (b) {
+  //   console.log(b);
+  //   a.addEventListener("click", function () {
+  //     // b.classList.add("active");
+  //     if (a.classList.contains("active")) {
+  //       b.classList.add("active");
+  //     } else {
+  //       b.classList.remove("active");
+  //     }
+  //   });
+  // });
 });
-
-// panel.forEach(function (b) {
-//   this.classList.toggle("active");
-//   console.log(this);
-// });
-// //1. 버튼 요소 선택
-// const btns = document.querySelectorAll(".btns .btn");
-// const panels = document.querySelectorAll(".panels .panel");
-
-// //4. 각각의 버튼 클릭 시 active 클래스 추가 : activeTabs 함수 정의
-// function activeTabs(idx) {
-//   //3번 과정에서 전달된 i 파라미터는 idx에 저장
-//   //   console.log(idx);
-//   //   console.log(btns[idx]);
-//   // 5번 :각각의 버튼을 반복문으로 추출하여 전체 요소의 active 클래스 제거
-//   for (let i = 0; i < btns.length; i++) {
-//     const btn = btns[i];
-//     btn.classList.remove("active");
-//   }
-//   //7번: 전체 패널을 반복문으로 각각 추출하여 display값 none 으로 변경(버튼 클릭시 실행)
-//   for (let i = 0; i < panels.length; i++) {
-//     panels[i].style.display = "none";
-//   }
-//   //8번 : 패널 3개 중 idx로 전달받은 패널의 요소애먼 display를 block으로 변경
-//   panels[idx].style.display = "block";
-//   // 6번 : 전달 받은 idx를 인덱스로 하는 버튼(클릭하는 대상)에만 active 클래스 추가
-//   btns[idx].classList.add("active");
-// }
-
-// //2. 반복문으로 버튼 요소 추출 : 버튼 개를 반복문으로 각각 추출한다.
-// for (let i = 0; i < btns.length; i++) {
-//   const btn = btns[i];
-//   //   console.log(btn);
-
-//   //3.  버튼 클릭 시 전체 active 클래스 삭제 : 각각 추출된 버튼 요소를 클릭했을 때 activeTabs함수 실행
-//   btn.addEventListener("click", () => {
-//     activeTabs(i); //함수의 파라미터로 i 인덱스 전달 ex) 첫번째 탭 클릭 시 0 전달, 두 번째 탭 클릭시 1 전달...
-//   });
-// }
