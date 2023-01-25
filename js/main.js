ba36ffd95752854fee4c6b7f265754b5120e9f78
shimyoujin <zipte@naver.com>
2023-01-25 22:08:08 +0900

    사이드바 완료

--- js/main.js
+++ js/main.js
@@ -190,3 +190,20 @@ function closeNav() {
 //     }
 //   });
 // });
+
+var accItem = document.getElementsByClassName("accordionItem");
+var accHD = document.getElementsByClassName("accordionItemHeading");
+for (i = 0; i < accHD.length; i++) {
+  console.log(accHD);
+  accHD[i].addEventListener("click", toggleItem);
+}
+function toggleItem() {
+  console.log(this.parentNode.parentNode);
+  var itemClass = this.parentNode.parentNode.className;
+  for (i = 0; i < accItem.length; i++) {
+    accItem[i].className = "accordionItem close";
+  }
+  if (itemClass == "accordionItem close") {
+    this.parentNode.parentNode.className = "accordionItem open";
+  }
+}
